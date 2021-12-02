942805c524d8434ee16089ceb7e673de4a5f95f0
armaBurton <armanarma@armaburton.com>
2021-12-01 16:05:13 -0800

    cleaned up utils.js

--- script/utils.js
+++ script/utils.js
@@ -4,18 +4,4 @@ export function makeStatsString(headCount, middleCount, bottomCount) {
     let bottom = bottomCount.toString();
     
     return head + middle + bottom;
-
-
-    // let headCounter = document.createElement(`p`);
-    // headCounter.innerHTML = `You have changed the head <span class="bolder">${headCount}</span> times.`;
-    // report.concat(headCounter);
-
-    // let middleCounter = document.createElement(`p`);
-    // middleCounter.innerHTML = `You have changed the middle <span class="bolder">${middleCount}</span> times.`;
-    // report.append(middleCounter);
-    
-    // let bottomCounter = document.createElement(`p`);
-    // bottomCounter.innerHTML = `You have changed the bottom <span class="bolder">${bottomCount}</span> times.`;
-    // report.append(bottomCounter);
-    // return report;
 }
